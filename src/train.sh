2975e886b5f6e022eb47a59f7d0e98dc8fae95a6
mylesz <mingsheng1712@163.com>
2025-11-14 11:15:26 +0800

    [update]

--- src/train.sh
+++ src/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 train.py -m data/attributes=2_NABirds,3_DTD,4_OxfordIIITPet,5_StanfordDogs,6_StanfordCars,7_CALTECH101,8_CALTECH256,9_GTSRB model.use_teacher=true,false
-
+python3 src/train.py -m data/attributes=6_StanfordCars model.use_teacher=true data.batch_size=256 data.num_workers=16 trainer.devices=1
 #0_CUB_200_2011,1_FGVC_AIRCRAFT,
